ae456bcf77fac1b587e34cc68c76a777d4ae59e6
Hesam Rahmani <hesamrahmanihrmn@gmail.com>
2022-10-18 13:03:37 +0330

    Deploying

--- app.js
+++ app.js
@@ -32,13 +32,17 @@ mongoose
       res.send("No1 Shop Api");
     });
     console.log(process.env.PORT);
-    app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
-      console.log(
-        "Express server listening on port %d in %s mode",
-        this.address().port,
-        app.settings.env
-      );
-    });
+    const PORT = process.env.PORT || "8000";
+
+    app.set("port", PORT);
+
+    // app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
+    //   console.log(
+    //     "Express server listening on port %d in %s mode",
+    //     this.address().port,
+    //     app.settings.env
+    //   );
+    // });
   })
   .catch((err) => {
     console.log(err);
