5ab1fc9a8da4192adbee0ce22eb8175526fe9b69
Hesam Rahmani <hesamrahmanihrmn@gmail.com>
2022-10-18 13:09:48 +0330

    Deploying

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const Routes = require("./routes/routes");
 const app = express();
 app.use(express.json({ extended: false }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "*");
+//   next();
+// });
 
-app.use(cors());
+// app.use(cors());
 
 app.use("/api", Routes);
 
