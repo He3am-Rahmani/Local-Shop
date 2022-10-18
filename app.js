14c7c3c8e3fc3f8a5d6d98b3abed4da9c4fd3952
Hesam Rahmani <hesamrahmanihrmn@gmail.com>
2022-10-18 13:26:11 +0330

    Deploying

--- app.js
+++ app.js
@@ -19,19 +19,6 @@ app.use(cors());
 
 app.use("/api", Routes);
 
-// `mongodb+srv://no1User:hesamrahmanihrmn@gmail.com@no1shop.5pnmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-mongoose
-  .connect(
-    `mongodb+srv://no1User:hesamrahmanihrmn%40gmail.com@no1shop.5pnmv.mongodb.net/LocalShop?retryWrites=true&w=majority`,
-    { useUnifiedTopology: true, useNewUrlParser: true }
-  )
-  // `${process.env.DB}://${process.env.USER}:${process.env.UPASSWORD}${process.env.DOMAIN}/LocalShop`
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.get("/", (req, res, next) => {
-      res.send("No1 Shop Api");
-    });
-    console.log(process.env.PORT);
     app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
       console.log(
         "Express server listening on port %d in %s mode",
@@ -39,7 +26,20 @@ mongoose
         app.settings.env
       );
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// `mongodb+srv://no1User:hesamrahmanihrmn@gmail.com@no1shop.5pnmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// mongoose
+//   .connect(
+//     `mongodb+srv://no1User:hesamrahmanihrmn%40gmail.com@no1shop.5pnmv.mongodb.net/LocalShop?retryWrites=true&w=majority`,
+//     { useUnifiedTopology: true, useNewUrlParser: true }
+//   )
+//   // `${process.env.DB}://${process.env.USER}:${process.env.UPASSWORD}${process.env.DOMAIN}/LocalShop`
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.get("/", (req, res, next) => {
+//       res.send("No1 Shop Api");
+//     });
+//     console.log(process.env.PORT);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
